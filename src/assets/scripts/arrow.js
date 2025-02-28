c48c257fe827bbd0e59c8ef2c7bd9c53feaf023d
Michael McCain <michaelmc1989@gmail.com>
2025-02-28 17:38:11 -0500

    agasdfasdfasdfasdf

--- src/assets/scripts/arrow.js
+++ src/assets/scripts/arrow.js
@@ -1,23 +1,21 @@
-window.onload = () => {
-  // Links the scene and arrow to global variables by finding the scene and arrow entities in the HTML Document
-  const scene = document.querySelector("a-scene")
+// Links the scene and arrow to global variables by finding the scene and arrow entities in the HTML Document
+const scene = document.querySelector("a-scene")
 
-  // Waits for the scene element to load, then create the arrow entity and display its properties in the console log.
-  scene.addEventListener("loaded", function () {
-    const elementArrow = document.getElementById("arrow")
-    console.log("Arrow entity:", elementArrow)
-  })
+// Waits for the scene element to load, then create the arrow entity and display its properties in the console log.
+scene.addEventListener("loaded", function () {
+  const elementArrow = document.getElementById("arrow")
+  console.log("Arrow entity:", elementArrow)
+})
 
-  // Custom component that logs users position in the world using THREE.js
-  AFRAME.registerComponent("rotation-reader", {
-    tick: (function () {
-      var position = new THREE.Vector3()
-      var quaternion = new THREE.Quaternion()
+// Custom component that logs users position in the world using THREE.js
+AFRAME.registerComponent("rotation-reader", {
+  tick: (function () {
+    var position = new THREE.Vector3()
+    var quaternion = new THREE.Quaternion()
 
-      return function () {
-        console.log(this.el.object3D.getWorldPosition(position))
-        console.log(this.el.object3D.getWorldQuaternion(quaternion))
-      }
-    })(),
-  })
-}
+    return function () {
+      console.log(this.el.object3D.getWorldPosition(position))
+      console.log(this.el.object3D.getWorldQuaternion(quaternion))
+    }
+  })(),
+})
