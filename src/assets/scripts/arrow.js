13b7890f076ee9413e052b8995245f05e9f7fe6f
Michael McCain <michaelmc1989@gmail.com>
2025-03-07 13:52:14 -0500

    adfasd

--- src/assets/scripts/arrow.js
+++ src/assets/scripts/arrow.js
@@ -40,20 +40,3 @@ window.addEventListener("gps-camera-update-position", function (e) {
   // Apply the quaternion to the arrow so it points correctly in camera space.
   arrowEl.object3D.quaternion.copy(targetQuaternion)
 })
-
-AFRAME.registerComponent("rotation-reader", {
-  /**
-   * We use IIFE (immediately-invoked function expression) to only allocate one
-   * vector or euler and not re-create on every tick to save memory.
-   */
-  tick: (function () {
-    var position = new THREE.Vector3()
-    var quaternion = new THREE.Quaternion()
-
-    return function () {
-      this.el.object3D.getWorldPosition(position)
-      this.el.object3D.getWorldQuaternion(quaternion)
-      // position and rotation now contain vector and quaternion in world space.
-    }
-  })(),
-})
