02d7b90eafd2ef892f140f9973ff4048155cf943
Michael McCain <michaelmc1989@gmail.com>
2025-03-09 20:37:38 -0400

    arrowTxt

--- src/assets/scripts/distance.js
+++ src/assets/scripts/distance.js
@@ -1,28 +1,29 @@
-window.addEventListener("load", () => {
-  const camera = document.querySelector("[camera]")
-  const marker = document.querySelector("a-marker")
-  let check
+AFRAME.registerComponent("distance-calc", {
+  init: function () {
+    this.arrowText = this.el // The entity doing the pointing (tagged element)
+    this.targetEl = document.querySelector("#event") // The entity we want to point toward
+    this.cameraEl = document.querySelector("a-camera") // POV using the user's camera
+  },
 
-  marker.addEventListener("markerFound", () => {
-    let cameraPosition = camera.object3D.position
-    let markerPosition = marker.object3D.position
-    let distance = cameraPosition.distanceTo(markerPosition)
+  tick: function () {
+    if (!this.targetEl || !this.cameraEl) return
 
-    check = setInterval(() => {
-      cameraPosition = camera.object3D.position
-      markerPosition = marker.object3D.position
-      distance = cameraPosition.distanceTo(markerPosition)
+    // World position of event (target)
+    const targetWorldPos = new THREE.Vector3()
+    this.targetEl.object3D.getWorldPosition(targetWorldPos)
 
-      // do what you want with the distance:
-      console.log(distance)
-    }, 100)
-  })
+    // World position of camera
+    const cameraPos = new THREE.Vector3()
+    this.cameraEl.object3D.getWorldPosition(cameraPos)
 
-  marker.addEventListener("markerLost", () => {
-    clearInterval(check)
-  })
-})
+    // Get manhattan distance from camera to target
+    let dist
+    dist = cameraPos.manhattanDistanceTo(targetWorldPos)
+    dist = dist.toFixed(2) // format to 2 decimal places
+    var distTxt =
+      "Your event is this way.\n Distance: " + dist.toString(dist) + "m" // Combine dist and text
 
-AFRAME.registerComponent("distance", {
-  tick: function () {},
+    // Update arrowText
+    this.arrowText.setAttribute("value", distTxt)
+  },
 })
