6d99c78da4772a2429dee6d6b884667a315a51a6
Michael McCain <michaelmc1989@gmail.com>
2025-04-06 10:31:15 -0400

    Updated display info component for test.

--- src/assets/scripts/interact.js
+++ src/assets/scripts/interact.js
@@ -1,8 +1,12 @@
 // interact.js
 // This componenet modifies an entity to do something when the user touches the screen location where the entity is displayed (i.e., user touches event box, box displays event information on screen, user touches again, event info goes away).
 
-AFRAME.registerComponent("display-info", {
+AFRAME.registerComponent("click-display-info", {
   init: function () {
-    console.log("interaction is working!")
+    var data = this.data
+    var el = this.el
+    this.el.addEventListener("click", function (evt) {
+      console.log(`Display-info is attached to ${el} !`)
+    })
   },
 })
