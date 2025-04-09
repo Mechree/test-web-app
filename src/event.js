80b045c85048bcb1573527b1299accdcd4aaabd7
Michael McCain <michaelmc1989@gmail.com>
2025-04-09 07:35:45 -0400

    remved event rotate from event.js.

--- src/event.js
+++ src/event.js
@@ -2,6 +2,7 @@
 import { getEventByTitle } from "./fetch-event.js"
 
 // Wait for the document to load, link scene and event entity to their respective variables, place event location via coordinates, and add some sort of marker.
+
 document.addEventListener("DOMContentLoaded", function () {
   const scene = document.querySelector("a-scene")
 
@@ -10,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const elementEvent = document.getElementById("#event")
 
       if (elementEvent) {
-        // console.log("Event entity found!")
+        console.log("Event entity found!")
 
         // Set scale, primitive and color.
         elementEvent.setAttribute("geometry", {
@@ -40,23 +41,4 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     console.error("Scene element not found!")
   }
-})
-
-// This component rotates the box at every frame tick (might add limiter to conserve resources)
-AFRAME.registerComponent("rotation-tick", {
-  init: function () {
-    this.rotationTmp = { x: 0, y: 0, z: 0 } // Properly initialize rotationTmp
-  },
-  tick: function () {
-    var el = this.el
-    var rotation = el.getAttribute("rotation")
-
-    // Increment the rotation
-    this.rotationTmp.x = rotation.x + 0.2
-    this.rotationTmp.y = rotation.y + 0.2
-    this.rotationTmp.z = rotation.z + 0.2
-
-    // Apply new rotation
-    el.setAttribute("rotation", this.rotationTmp)
-  },
 })
